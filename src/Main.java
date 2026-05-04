01fd9dde22cd851f2f53eb649520fdb69bb85623
Rodny Paredes <rodnyparedesgg@gmail.com>
2026-05-04 11:05:41 -0500

    Version 2.0 Contrutores CLases Poo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 void main() {
     System.out.println("Hola Mundo");
+    Persona persona1 = new Persona();
+    System.out.println("Persona1: Nombre = " + persona1.getNombre());
+    System.out.println("persona1: Edad   = " + persona1.getEdad());
+    System.out.println("\n");
+    persona1.setEdad(25);
+    persona1.setNombre("El Juanito");
+    System.out.println("Persona1: Nombre = " + persona1.getNombre());
+    System.out.println("persona1: Edad   = " + persona1.getEdad());
 }
---
+++ src/Persona.java
@@ -0,0 +1,29 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(){
+        this.nombre="Sin nombre";
+        this.edad=0;
+        //System.out.println("Obejeto Persina creado con valores prodefecto");
+    }
+    //get obtener
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Set Asiganar
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
