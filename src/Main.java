51c18a5b294506068b5af5301627ea9384c34b8e
Carlos <andreskr942@gmail.com>
2026-05-04 11:16:17 -0500

    Puse hola mundo

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,5 @@ void main() {
     persona1.setNombre("El Juanito");
     System.out.println("Persona1: Nombre = " + persona1.getNombre());
     System.out.println("persona1: Edad   = " + persona1.getEdad());
+    System.out.println(" Hola Mundo");
 }
--- src/Persona.java
+++ src/Persona.java
@@ -26,4 +26,5 @@ public class Persona {
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
 }
